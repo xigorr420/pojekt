3c117953318063e8defba2a93cdd3f9401684f0f
xigorr420 <ksiazek03@gmail.com>
2025-01-08 12:16:31 +0100

    pierwszy

---
+++ pojekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pojekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
